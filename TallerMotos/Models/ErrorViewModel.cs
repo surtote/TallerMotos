b415fcf8fcec5bb9eb984455e4dd14e6785e577f
Jose Luis Gardeta <prutote@gmail.com>
2021-04-19 12:42:38 +0200

    1st commit Signed-off-by: Jose Luis Gardeta <prutote@gmail.com>

---
+++ TallerMotos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TallerMotos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
